ab2a960a0a16868c43d9417472fbb41927f3dae2
Elmer Vasquez <elmer.vasquez@mangoodev.com>
2022-08-01 19:40:57 -0300

    Proyecto configurado, falta hibernate

--- TP_INTEGRADOR_G5/src/controller/MenuController.java
+++ TP_INTEGRADOR_G5/src/controller/MenuController.java
@@ -4,16 +4,16 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import servicio.IMenuService;
+//import servicio.IMenuService;
 
 @Controller
 public class MenuController {
 	
-	IMenuService _menuService;
-	
-	public MenuController(IMenuService menuService) {
-		_menuService = menuService;
-	}
+//	IMenuService _menuService;
+//	
+//	public MenuController(IMenuService menuService) {
+//		_menuService = menuService;
+//	}
 
 	@RequestMapping("menu_principal.html")
 	public ModelAndView getMenuPrincipal(String usuario, String password) 
---
+++ TP_INTEGRADOR_G5/src/dominio/Biblioteca.java
@@ -0,0 +1,82 @@
+//package dominio;
+//
+//import java.io.Serializable;
+//import java.util.Date;
+//
+//import javax.persistence.CascadeType;
+//import javax.persistence.Entity;
+//import javax.persistence.FetchType;
+//import javax.persistence.GeneratedValue;
+//import javax.persistence.GenerationType;
+//import javax.persistence.Id;
+//import javax.persistence.JoinColumn;
+//import javax.persistence.ManyToOne;
+//
+//@Entity
+//public class Biblioteca implements Serializable {
+//	
+//	private static final long serialVersionUID = 1L;
+//
+//	@Id
+//	@GeneratedValue(strategy = GenerationType.IDENTITY)
+//	private int id;
+//	
+//	@ManyToOne(cascade= {CascadeType.ALL}, fetch = FetchType.EAGER)
+//	@JoinColumn(name="idLibro")
+//	private Libro libro;
+//	
+//	private Date fechaAlta;
+//	
+//	private int estado;
+//	
+//	public Biblioteca() {}
+//	
+//	public Biblioteca(int id, Libro libro, Date fechaAlta, int estado) {
+//		super();
+//		this.id = id;
+//		this.libro = libro;
+//		this.fechaAlta = fechaAlta;
+//		this.estado = estado;
+//	}
+//
+//	public int getId() {
+//		return id;
+//	}
+//
+//	public void setId(int id) {
+//		this.id = id;
+//	}
+//
+//	public Libro getLibro() {
+//		return libro;
+//	}
+//
+//	public void setLibro(Libro libro) {
+//		this.libro = libro;
+//	}
+//
+//	public Date getFechaAlta() {
+//		return fechaAlta;
+//	}
+//
+//	public void setFechaAlta(Date fechaAlta) {
+//		this.fechaAlta = fechaAlta;
+//	}
+//
+//	public int getEstado() {
+//		return estado;
+//	}
+//
+//	public void setEstado(int estado) {
+//		this.estado = estado;
+//	}
+//	
+//	public String GetDatosPunto2() {
+//		return "\nId: " + id + "\nFecha alta: " + fechaAlta + "\nTitulo libro: " + libro.getTitulo() + "\n";
+//	}
+//
+//	@Override
+//	public String toString() {
+//		return "\nBiblioteca [id=" + id + ", libro=" + libro + ", fechaAlta=" + fechaAlta + ", estado=" + estado + "]";
+//	}
+//}
