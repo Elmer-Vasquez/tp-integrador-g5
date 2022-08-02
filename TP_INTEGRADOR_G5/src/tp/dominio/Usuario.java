4d883f1b8fe6f59a8f4540657bdd2d9434c5f92e
Elmer Vasquez <elmer.vasquez@mangoodev.com>
2022-08-02 20:50:52 -0300

    UsuarioJava

--- TP_INTEGRADOR_G5/src/tp/dominio/Usuario.java
+++ TP_INTEGRADOR_G5/src/tp/dominio/Usuario.java
@@ -1,5 +1,47 @@
 package tp.dominio;
 
-public class Usuario {
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Usuario implements Serializable {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	private String email;
+	
+	private String password;
+	
+	public Usuario() {}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
 
 }
